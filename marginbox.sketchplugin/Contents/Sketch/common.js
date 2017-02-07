4570fc3db3fb3bb7e3f9a9ad705def7b1e2c9cae
Francis <hisconer@gmail.com>
2017-02-07 18:06:26 +0100

    Refactor the function using ternary

--- marginbox.sketchplugin/Contents/Sketch/common.js
+++ marginbox.sketchplugin/Contents/Sketch/common.js
@@ -4,11 +4,5 @@ const findLayersByName = (name, scope) => scope.filteredArrayUsingPredicate(
 );
 
 // Get the top level group
-const getRootGroup = layer => {
-    const parent = layer.parentGroup()
-    if (parent.class() == "MSArtboardGroup") {
-        return layer;
-    } else {
-        return getRootGroup(parent);
-    }
-}
+const rootGroup = layer =>
+  layer.parentGroup().class() == "MSArtboardGroup" ? layer : rootGroup(layer.parentGroup());
