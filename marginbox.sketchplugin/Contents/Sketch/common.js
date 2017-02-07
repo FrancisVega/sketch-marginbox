4e3c136fc07a8034368021d8ef3f9e3988cd6286
Francis <hisconer@gmail.com>
2017-02-07 18:01:32 +0100

    Add parentRoot func

--- marginbox.sketchplugin/Contents/Sketch/common.js
+++ marginbox.sketchplugin/Contents/Sketch/common.js
@@ -2,3 +2,13 @@
 const findLayersByName = (name, scope) => scope.filteredArrayUsingPredicate(
   NSPredicate.predicateWithFormat('name == %@', name),
 );
+
+// Get the top level group
+const getRootGroup = layer => {
+    const parent = layer.parentGroup()
+    if (parent.class() == "MSArtboardGroup") {
+        return layer;
+    } else {
+        return getRootGroup(parent);
+    }
+}
